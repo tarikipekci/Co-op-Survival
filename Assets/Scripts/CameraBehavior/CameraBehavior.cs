1f92664dc38b5b53859097c8db1b944fad7c1b50
Yilmaz Tarik Ipekci <ytarik.ipekci@gmail.com>
2025-07-26 13:53:40 +0300

    Added -> Map Borders

--- Assets/Scripts/CameraBehavior/CameraBehavior.cs
+++ Assets/Scripts/CameraBehavior/CameraBehavior.cs
@@ -9,11 +9,20 @@ namespace CameraBehavior
         public float damping = 0.25f;
         private Vector3 _velocity = Vector3.zero;
 
+        public float minX = -9f;
+        public float maxX = 9f;
+        public float minY = -14f;
+        public float maxY = 14f;
+
         private void LateUpdate()
         {
             if (target == null) return;
 
             Vector3 targetPosition = target.position + offset;
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minX, maxX);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minY, maxY);
+
             transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, damping);
         }
 
